b5d0a1554f06d41e1ac79a53741653d711669130
itthardik <hardik.sharma@intimetec.com>
2024-10-29 10:09:50 +0530

    added readme and gitignore

--- obj/Debug/net8.0/DashboardUnitTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/DashboardUnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardUnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b3ceac6a12e41d565bcc912786fd2ebe9c2349")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardUnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardUnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
